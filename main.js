67aa2a2af3fcccf0d81cb174a7ebf9065bfb5431
Juli <julieta.bogado@outlook.es>
2024-06-03 19:48:50 -0300

    comiteando

--- main.js
+++ main.js
@@ -1,82 +1,132 @@
-let nombre = prompt("Ingresa nombre: ");
-
-function saludar(nombre){
-    console.log("Bienvenido/a " + nombre);
-}
-
-saludar(nombre);
-
-
-
-const motovehiculos = [
-    {
-        marca: "Honda",
-        modelo: "New Twister",
-        cc: 250,
-        precio: 3000
-    },
+const motosArray = [
 {
-    marca: "Yamaha",
-    modelo: "Ybr",
-    cc: 125,
-    precio: 1800
+   nombre : "Motomel s2",
+   marca : "Motomel",
+   modelo: "Serie 2",
+   precio: 1600000,
+   img: "img/349968-800-auto.png"
+    
 },
+
 {
-    marca: "Motomel",
+    nombre : "Motomel deluxe",
+    marca : "Motomel",
     modelo: "DLX",
-    cc: 110,
-    precio: 1200
-},
-{
-    marca: "Bajaj",
-    modelo: "Boxer",
-    cc: 150,
-    precio: 1600
-}
+    precio: 1300000,
+    img: "img\MOTOMEL---DLX-110.png" 
+ },
+
+ {
+    nombre : "Motomel blitz tunning",
+    marca : "Motomel",
+    modelo: "Blitz tunning",
+    precio: 1400000 ,
+    img: "img\Blitz-110-v8.II_Tunning_white_web.png" 
+ },
+
+ {
+    nombre : "Motomel Skua silver",
+    marca : "Motomel",
+    modelo: "Skua silver",
+    precio: 2200000,
+    img: "https://images.unomotos.com.ar/unomotos/motos_mini/motomel_skua_150_silver_0.png" 
+ },
+
+ {
+    nombre : "Honda GLH",
+    marca : "Honda",
+    modelo: "GLH",
+    precio: 5200000,
+    img: "img\honda_glh_150_gaucha_mini.png" 
+ },
+
+ {
+    nombre : "Rouser NS 200",
+    marca : "Bajaj",
+    modelo: "NS",
+    precio: 4000000,
+    img: "img\bajaj_rouser_200_ns_0km_mini.png"
+ },
+
+ {
+    nombre : "Rouser NS 160",
+    marca : "Bajaj",
+    modelo: "NS",
+    precio: 3300000,
+    img: "img\bajaj-pulsar-ns160-td-abs-ug-2024-negro-a68347.png" 
+ },
+
+ {
+    nombre : "Honda cb",
+    marca : "Honda",
+    modelo: "CB",
+    precio:6000000, 
+    img: "https://motos.honda.com.co/images/cms/Nueva-CB-300F-rojo.png" 
+ },
+
+ {
+    nombre : "Honda crf",
+    marca : "Honda",
+    modelo: " CRF",
+    precio: 13300000,
+    img: "img\c2_r (1).png"
+ },
+
+ {
+    nombre : "Yamaha cryton",
+    marca : "Yamaha",
+    modelo: "Crypton",
+    precio: 2300000,
+    img: "img\CryptonFi_motos_header.png"
+ }
 ];
 
-const adicionales = [
-    {
-        nombre: "Guantes",
-        precio: 100
-    },
-    {
-        nombre: "Casco",
-        precio: 400
-    },
-    {
-        nombre: "Piloto",
-        precio: 350
-    }
-]
+const motos = document.querySelectorAll(".moto");
+const carritoCantidad = document.getElementById("cantidad-carrito");
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const actualizarCarrito = () => {
+   carritoCantidad.textContent = carrito.length;
+   localStorage.setItem("carrito", JSON.stringify(carrito));
+};
 
 
-
-const precioConCredito = motovehiculos.map((moto) => {
-    return{
-        marca: moto.marca,
-        modelo: moto.modelo,
-        cc: moto.cc,
-        precio: moto.precio * 1.5
-    }
+motos.forEach(moto => {
+   const botonComprar = moto.querySelector(".agregar");
+   botonComprar.addEventListener("click", () => {
+      const nombre = moto.querySelector(".nombre-moto").textContent;
+      const precio = moto.querySelector(".precio-moto").textContent;
+      carrito.push({nombre, precio});
+      actualizarCarrito();
+   });
 });
 
+function renderMotos(motosToRender) {
+   contenedorMotos.innerHTML = '';
+   motosToRender.forEach(moto => {
+       const motoElement = document.createElement('div');
+       motoElement.classList.add('moto');
+       motoElement.innerHTML = `
+           <img id="${moto.id}" class="img-moto" src="${moto.img}" alt="${moto.nombre}">
+           <div class="details">
+               <h3 class="nombre-moto">${moto.nombre}</h3>
+               <p class="precio-moto">$${moto.precio.toLocaleString()}</p>
+               <button class="agregar">COMPRAR</button>
+           </div>
+       `;
+       contenedorMotos.appendChild(motoElement);
+      })}
 
-function metodoDePago(){
-   while(true){
-    let pago = prompt("INGRESE METODO DE PAGO EFT O TJA:").toUpperCase();
-    if (pago === "EFT"){
-        console.log("ESTOS SON LOS PRECIOS AL CONTADO: ")
-        console.log(motovehiculos);
-        break
-    }else if(pago === "TJA"){
-        console.log("ESTOS SON LOS PRECIOS DE LISTA: ")
-        console.log(precioConCredito);
-        break
-    }else{
-        alert("DATO INVALIDO. INGRESE NUEVAMENTE EFT O TJA")
-    }
-   }
-    
+document.getElementById("honda-boton").addEventListener("click", () => filtrarMotos("HONDA"));
+document.getElementById("yamaha-boton").addEventListener("click", () => filtrarMotos("YAMAHA"));
+document.getElementById("motomel-boton").addEventListener("click", () => filtrarMotos("MOTOMEL"));
+document.getElementById("bajaj-boton").addEventListener("click", () => filtrarMotos("BAJAJ"));
+
+function filtrarMotos(marca){
+   const motosFiltradas = motos.filter(moto => moto.marca === marca);
+
 }
-metodoDePago();
+
+renderMotos(filtrarMotos);
+
+actualizarCarrito();
